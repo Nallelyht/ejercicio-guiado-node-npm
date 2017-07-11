f25fd940d80b27b420a00f14509904f21af3bd14
Nallely House <nallelyht@gmail.com>
2017-07-11 12:32:24 -0500

    primer commit

---
+++ server.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var path = require('path');
+var app = express();
+
+//app.get("/otraliga", function(req,res){
+//  res.sendFile(__dirname + '/index.html');
+//});
+
+//app.listen(8080);
+app.get('/', (req, res) => {
+  res.sendFile(__dirname+'/index.html');
+});
+app.listen(8080);
+
+app.use('/static', express.static(path.join(__dirname, 'node_modules')));
+app.use('/static', express.static(path.join(__dirname,'assets')));
